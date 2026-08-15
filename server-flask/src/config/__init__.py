82b7d687912e635291a3ce263f1e63ed4687742e
dekoov <dcobel@hotmail.com>
2026-08-15 00:41:07 -0500

    refactor: mejora de legibilidad y actualizacion de gitignore

--- server-flask/src/config/__init__.py
+++ server-flask/src/config/__init__.py
@@ -1,6 +1,6 @@
 from .config import config
 from .deploy_config import resolve_route
-from .playwright_config import check_playwright
 from .logging_config import setup_logging
+from .playwright_config import check_playwright
 
-__all__ = ['config', 'check_playwright', 'resolve_route', 'setup_logging']
+__all__ = ['check_playwright', 'config', 'resolve_route', 'setup_logging']
